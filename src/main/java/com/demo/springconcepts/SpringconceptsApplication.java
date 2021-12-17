ac7ebdd7d7fbc5b7f7c19e6ae072e45fb5aa44f8
ravi <ravi@gmail.com>
2021-12-17 23:45:52 +0530

    Added xml file logger object

--- src/main/java/com/demo/springconcepts/SpringconceptsApplication.java
+++ src/main/java/com/demo/springconcepts/SpringconceptsApplication.java
@@ -1,20 +1,23 @@
 package com.demo.springconcepts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.demo.springconcepts.component.DemoBean;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 /**
  * @SpringBootApplication to declare class main app class
  * ApplicationContext to configure SpringConceptApplication
+ * Logger object to Loging messages of SpringConceptsApplication 
  */
 @SpringBootApplication
 public class SpringconceptsApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SpringconceptsApplication.class);
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concept Demo");
+		logger.debug("Welcome to Spring Concept Demo");
 		ApplicationContext context = SpringApplication.run(SpringconceptsApplication.class, args);
-		System.out.println("Checking Context : "+context.getBean(DemoBean.class));
+		logger.debug("Checking Context : "+context.getBean(DemoBean.class));
 	}
 
 }
